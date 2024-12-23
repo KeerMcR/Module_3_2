8857f20f249e9898d5269d37a330e199a1a0e845
KeerMcR <119884975+KeerMcR@users.noreply.github.com>
2024-12-23 19:33:40 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -3,7 +3,7 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
             or not recipient.endswith(('.com', '.net', '.ru')) \
             or not sender.endswith(('.com', '.net', '.ru')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif recipient is sender:
+    elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
     elif sender == "university.help@gmail.com":
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
